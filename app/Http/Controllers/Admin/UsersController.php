09534d6b9ebcca26ceee2544f1c5b95b98a42006
MuhaddiMu <muhaddisshah@gmail.com>
2019-04-26 12:20:23 +0500

    Assigning User Functionality Created Successfully

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Spatie\Permission\Models\Role;
+use App\Http\Requests\UserEditFormRequest;
+use Illuminate\Support\Facades\Hash;
 use App\User;
 
 class UsersController extends Controller
@@ -16,6 +18,26 @@ class UsersController extends Controller
     }
 
     public function Edit($ID){
-        return view('Admin/Users/Edit');
+        $User = User::whereId($ID)->first();
+        $Roles = Role::all();
+        $SelectedRoles = $User->roles()->pluck('name')->toArray();
+        return view('Admin/Users/Edit')->with('User', $User)->with('Roles', $Roles)->with('SelectedRoles', $SelectedRoles);
+    }
+
+    public function Update($ID, UserEditFormRequest $Request){
+        $User = User::whereId($ID)->first();
+        $User->name = $Request->input('Name');
+        $User->email = $Request->input('Email');
+        $Password = $Request->input('Password');
+
+            if($Password != ''){
+                $User->password = Hash::make($Password);
+            }
+
+        $User->save();
+
+        $User->syncRoles($Request->input('Role'));
+
+        return redirect(action([UsersController::class, 'Edit'], $User->id) )->with('status', 'User has been updated');
     }
 }
--- resources/views/Admin/Users/Edit.blade.php
+++ resources/views/Admin/Users/Edit.blade.php
@@ -1,1 +1,57 @@
-TEST
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Edit User</div>
+
+                <div class="card-body">
+
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            @include('layouts/errors')
+                        </div>
+                    @endif
+                    
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+					<form action="" method="post">
+						@csrf
+						<div class="form-group">
+							<input type="text" value="{{$User->name}}" name="Name" class="form-control" placeholder="Name">
+						</div>
+						<div class="form-group">
+								<input type="email" value="{{$User->email}}" name="Email" class="form-control" placeholder="Email">
+							</div>
+						<div class="form-group">
+							<select name="Role[]" class="form-control">
+								@foreach ($Roles as $Role)
+								<option value="{{ $Role->name }}" @if(in_array($Role->name, $SelectedRoles)) selected @endif >{{ $Role->name }}</option>							
+								@endforeach
+							</select>
+						</div>
+						<div class="form-group">
+							<input type="password" name="password" class="form-control" placeholder="Password">
+						</div>
+						<div class="form-group">
+								<input type="password" name="password_confirmation" class="form-control" placeholder="Confirm Password">
+						</div>
+						
+						<button type="submit" class="btn btn-primary">Submit</button>
+
+					</form>
+
+					
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
