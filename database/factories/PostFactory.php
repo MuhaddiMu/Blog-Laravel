bb3c46ec96f0807aef1debc99e9c0d3d00d6ae88
MuhaddiMu <muhaddisshah@gmail.com>
2019-05-01 19:10:02 +0500

    Seeder Added

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Post;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence(10),
+        'slug' => str_random(15),
+        'content' => $faker->sentence(1000),
+        'category_id' => 5,
+    ];
+});
